c2eca3a30cd5ee6a04d590d6960c858c4bc65764
mikemikemao <874754143@qq.com>
2022-03-01 16:27:01 +0800

    add recycleview

--- app/src/main/java/com/hikvision/ffmpegdemo/MainActivity.java
+++ app/src/main/java/com/hikvision/ffmpegdemo/MainActivity.java
@@ -1,16 +1,34 @@
 package com.hikvision.ffmpegdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.TextView;
+
+import com.hikvision.ffmpegdemo.adapter.SelectItem;
+import com.hikvision.ffmpegdemo.adapter.SelectItemAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private List<SelectItem> selectItemList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((TextView)findViewById(R.id.text_view)).setText("FFmpeg 版本和编译配置信息\n\n" + ZZFFmpeg.GetFFmpegVersion());
+        initItems();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        SelectItemAdapter adapter = new SelectItemAdapter(selectItemList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initItems() {
+        SelectItem foregroundItem = new SelectItem("foreground", R.drawable.ic_launcher_foreground);
+        selectItemList.add(foregroundItem);
+
     }
 }
---
+++ app/src/main/java/com/hikvision/ffmpegdemo/adapter/SelectItemAdapter.java
@@ -0,0 +1,50 @@
+package com.hikvision.ffmpegdemo.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hikvision.ffmpegdemo.R;
+
+import java.util.List;
+
+public class SelectItemAdapter extends RecyclerView.Adapter<SelectItemAdapter.ViewHolder> {
+    private List<SelectItem> mSelectItemList;
+
+    public SelectItemAdapter(List<SelectItem> selectItemList) {
+        mSelectItemList = selectItemList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.select_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        SelectItem mSelectItem = mSelectItemList.get(position);
+        holder.selectItemImage.setImageResource(mSelectItem.getImageId());
+        holder.selectItemName.setText(mSelectItem.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSelectItemList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView selectItemImage;
+        TextView selectItemName;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            selectItemImage = (ImageView) itemView.findViewById(R.id.select_image);
+            selectItemName = (TextView) itemView.findViewById(R.id.select_name);
+        }
+    }
+}
