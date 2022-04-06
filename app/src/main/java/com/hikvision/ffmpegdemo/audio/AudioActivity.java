380ec56a3c1c27e5c7a9ca44204fae2a40d2fbf6
mikemikemao <maochaoqun@hikvision.com>
2022-04-06 20:34:23 +0800

    add getAudioCurve

--- app/src/main/java/com/hikvision/ffmpegdemo/audio/AudioActivity.java
+++ app/src/main/java/com/hikvision/ffmpegdemo/audio/AudioActivity.java
@@ -1,8 +1,14 @@
 package com.hikvision.ffmpegdemo.audio;
 
+import android.content.res.AssetManager;
+import android.media.AudioDeviceInfo;
+import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hikvision.ffmpegdemo.R;
@@ -14,17 +20,31 @@ import com.hikvision.ffmpegdemo.R;
  * date : 2022/4/6 10:48
  */
 public class AudioActivity extends AppCompatActivity implements View.OnClickListener{
-
+    private static final String TAG = "AudioActivity";
+    AssetManager assetManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio);
         findViewById(R.id.tv_playPCM).setOnClickListener(this);
+        findViewById(R.id.tv_getAudioCurve).setOnClickListener(this);
+        assetManager=getAssets();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.tv_playPCM:
+                break;
+            case R.id.tv_getAudioCurve://API28 android9以上能用 以下用dumpsys media.audio_policy代替
+                AudioManager audioManager = (AudioManager) getSystemService(this.AUDIO_SERVICE);
+                int minIndex = audioManager.getStreamMinVolume (AudioManager.STREAM_MUSIC);
+                int maxIndex = audioManager.getStreamMaxVolume (AudioManager.STREAM_MUSIC);
+                for(int i=minIndex+1; i<maxIndex; i++) {
+                    float db = audioManager.getStreamVolumeDb(AudioManager.STREAM_MUSIC, i, AudioDeviceInfo.TYPE_WIRED_HEADSET);
+                    Log.d(TAG, "volume db = " + db + "  at index= " + i);
+                }
                 break;
             default:
                 break;
